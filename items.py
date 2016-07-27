3e53ff9c06a541c54cdf3a0b0009be19a10fc74b
Rob Sliwa <robjsliwa@gmail.com>
2016-07-27 01:54:10 +0000

    Updated item object to include weight and size

--- items.py
+++ items.py
@@ -1,12 +1,14 @@
 class Item():
 	"""The base class for all items"""
-	def __init__(self, name, description, value):
+	def __init__(self, name, description, value, weight, size):
 		self.name = name
 		self.description = description
 		self.value = value
+		self.weight = weight
+		self.size = size
 		
 	def __str__(self):
-		return "%s\n=====\n%s\nValue: %s" % (self.name, self.description, self.value)
+		return "%s\n=====\n%s\nValue: %s\nWeight: %s\nSize: %s" % (self.name, self.description, self.value, self.weight, self.size)
 		
 class Weapon(Item):
 	def __init__(self, name, description, value, damage):
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from items import *
 
-newItem = Item("Test", "blah blas", "20")
+newItem = Item("Test", "blah blas", "20", "10kg", "small")
 print newItem
