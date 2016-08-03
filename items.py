3f6698da80ac5e5f445cc1d38b2d96278da066e7
Rob Sliwa <robjsliwa@gmail.com>
2016-08-03 02:06:47 +0000

    Basic item implementation

--- items.py
+++ items.py
@@ -8,14 +8,44 @@ class Item():
 		self.size = size
 		
 	def __str__(self):
-		return "%s\n=====\n%s\nValue: %s\nWeight: %s\nSize: %s" % (self.name, self.description, self.value, self.weight, self.size)
+		return "%s\n=====\n%s\nValue: %s\nWeight: %s\nSize: %s\n" % (self.name, self.description, self.value, self.weight, self.size)
 		
 class Weapon(Item):
-	def __init__(self, name, description, value, damage):
+	def __init__(self, name, description, value, weight, size, damage):
 		self.damage = damage
-		super().__init__(name, description, value)
+		Item.__init__(self, name, description, value, weight, size)
 		
 	def __str__(self):
-		return "%s\n=====\n%s\nValue: %s\nDamage: %s\n" % (self.name, self.description, self.value, self.damage)
+		return "%s\n=====\n%s\nValue: %s\nDamage: %s\n\n" % (self.name, self.description, self.value, self.damage)
 		
+
+class ItemContainer(Item):
+	"""The Item Container"""
+	def __init__(self, name, description, value, weight, size):
+		if size == "small":
+			self.maxSize = 3
+		elif size == "medium":
+			self.maxSize = 10
+		else:
+			self.maxSize = 20
+			
+		self.container = []
+		
+		Item.__init__(self, name, description, value, weight, size)
+		
+	def isEmpty():
+		return len(self.container) == 0
+		
+	def addItem(self, item):
+		self.container.append(item)
+		
+	def removeItem(self, item):
+		self.container.remove(item)
+		
+	def __str__(self):
+		description = "%s contains:\n" % (self.name)
+		for item in self.container:
+			description += item.__str__()
+			description += "\n"
+		return description
 		
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from items import *
 
-newItem = Item("Test", "blah blas", "20", "10kg", "small")
-print newItem
+newItem1 = Item("Test1", "blah blas", "20", "10kg", "small")
+print newItem1
+
+newItem2 = Item("Test2", "blah blas", "20", "10kg", "small")
+print newItem2
+
+newItem3 = Item("Test3", "blah blas", "20", "10kg", "small")
+print newItem3
+
+weapon1 = Weapon("Sword", "Cool sword", "50", "12kg", "small", "12")
+print weapon1
+
+newContainer = ItemContainer("TestContainer", "blah blas", "20", "10kg", "small")
+newContainer.addItem(newItem1)
+newContainer.addItem(newItem2)
+newContainer.addItem(weapon1)
+
+print newContainer
